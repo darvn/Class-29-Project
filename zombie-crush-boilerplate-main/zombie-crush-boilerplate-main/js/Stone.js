2fdf25c7ef6b7e7838e6889a322eefa4896dee07
darvn <86173198+darvn@users.noreply.github.com>
2021-08-28 14:52:52 -0600

    Add files via upload

---
+++ zombie-crush-boilerplate-main/zombie-crush-boilerplate-main/js/Stone.js
@@ -0,0 +1,19 @@
+class Stone {
+    constructor(x, y, r){
+        var options = {
+            restitution : 0.8
+        }
+
+
+        this.body = Bodies.circle(x, y, r, options);
+        this.r = r
+        World.add(world, this.body)
+    }
+
+    show(){
+        var pos = this.body.position;
+        ellipseMode(RADIUS);
+        ellipse(pos.x, pos.y, this.r);
+    }
+
+}
